ad61eb09242d2ac4c0babf2c09794640569eaea5
Fabio Cervatti <cervattifabio@gmail.com>
2023-08-29 22:22:30 -0300

    Alteração e Melhorias 1

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult admin()
+        {
+            return View();
+        }
+
+        public IActionResult Sair()
+        {
+            HttpContext.Session.Clear();    
+            return RedirectToAction("Login", "Home");
+        }
+
+        public IActionResult RegistrarUsuarios()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaUsuarioAdmin(this);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistrarUsuarios(Usuario u)
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaUsuarioAdmin(this);
+
+            u.Senha = Criptografo.TextoCriptografado(u.Senha);
+
+            UsuarioService us = new UsuarioService();
+            us.incluirUsuario(u);
+            return RedirectToAction("ListarUsuarios");
+        }
+
+        public IActionResult ListarUsuarios()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaUsuarioAdmin(this);
+
+            UsuarioService us = new UsuarioService();
+
+            return View(us.Listar());
+        }
+
+        public IActionResult EditarUsuarios(int id)
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaUsuarioAdmin(this);
+
+            UsuarioService us = new UsuarioService();
+
+            return View(us.ListarId(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditarUsuarios(Usuario uEditado)
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaUsuarioAdmin(this);
+
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                Usuario u = new Usuario();
+                u = bc.Usuarios.Find(uEditado.Id);
+
+                if(u.Senha != uEditado.Senha)   
+                {
+                    uEditado.Senha = Criptografo.TextoCriptografado(uEditado.Senha);
+                }
+            }
+
+            UsuarioService us = new UsuarioService();
+            us.editarUsuario(uEditado);
+
+            return RedirectToAction("ListarUsuarios");
+        }
+
+
+        public IActionResult ExcluirUsuario(int id)
+        {
+            UsuarioService us = new UsuarioService();
+
+            us.excluirUsuario(id);
+
+            return RedirectToAction("ListarUsuarios");
+        }
+
+    }
+}
